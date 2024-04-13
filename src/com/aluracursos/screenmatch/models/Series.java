f32e2fb7b56379cfeaf9ed4bc7b1ae5b9caffee1
lfgarciaoc <luizafernandagarcia@gmail.com>
2024-04-13 17:46:33 -0500

    refactor: create super class

--- src/com/aluracursos/screenmatch/models/Series.java
+++ src/com/aluracursos/screenmatch/models/Series.java
@@ -1,2 +1,31 @@
-package com.aluracursos.screenmatch.models;public class Series {
+package com.aluracursos.screenmatch.models;
+
+public class Series extends Title {
+    int season;
+    int episodesSeason;
+    int minutesEpisode;
+
+    public int getSeason() {
+        return season;
+    }
+
+    public void setSeason(int season) {
+        this.season = season;
+    }
+
+    public int getEpisodesSeason() {
+        return episodesSeason;
+    }
+
+    public void setEpisodesSeason(int episodesSeason) {
+        this.episodesSeason = episodesSeason;
+    }
+
+    public int getMinutesEpisode() {
+        return minutesEpisode;
+    }
+
+    public void setMinutesEpisode(int minutesEpisode) {
+        this.minutesEpisode = minutesEpisode;
+    }
 }
